883b3825d8745ab687c4d99984350f3b80fc7ffd
Person man <personmanispersonman@gmail.com>
2024-06-28 21:08:30 -0400

    i don't know last minute stuff i suppose

--- boorish/settings.py
+++ boorish/settings.py
@@ -17,18 +17,19 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_ROOT = str(BASE_DIR / 'gallery' / 'images')
-APPEND_SLASH = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ea9l_9f_5rkwh*-q(*+285z#ev4z9)h5f$0gsaje#l0en%5eg&'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+                'genericgamergamed.mooo.com',
+                 ]
 
 
 # Application definition
--- gallery/urls.py
+++ gallery/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 
 app_name = "gallery"
+# I cut out this: path("<int:image_id>", views.detail, name="detail"),
 urlpatterns = [
     path("", views.unfiltered, name="index"),
     path("untagged/", views.untagged, name="untagged"),
-    path("<int:image_id>/", views.detail, name="detail"),
     path("upload/", views.upload, name="upload"),
     path("tags/", views.tags, name="tags"),
     path("a/add_tag", views.aj_add_tag, name="ajax add tag to image"),
